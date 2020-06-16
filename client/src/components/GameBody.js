6bb4abad5e41ce4b16f0669efc08a44ce308460b
H3kapoo <andrei.boje00@e-uvt.ro>
2020-06-16 19:09:35 +0300

    Finished handleResult on choice click

--- client/src/components/GameBody.js
+++ client/src/components/GameBody.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { generateChoice } from '../utils/utils.js'
 import './GameBody.css'
 
@@ -6,14 +6,46 @@ import './GameBody.css'
 
 const GameBody = () => {
 
+    //0 rock 1 paper 2 scissors
+    let possibleChoices = [0, 1, 2]
+
     const [userScore, setUserScore] = useState(0);
     const [compScore, setCompScore] = useState(0);
+    const [flag, setFlag] = useState("|");
 
-    //test
-    let choices = [1, 4, 5, 2, 6, 3]
-    //console.log(generateChoice(choices))
+    const [clickToggle, setClickToggle] = useState(false);
 
-    const handleResult = (p) => console.log(generateChoice(choices))
+
+    const handleResult = (userChoice) => {
+        let compChoice = generateChoice(possibleChoices)
+
+        switch (userChoice) {
+            case 0:
+                if (compChoice === 0) setFlag("TIE")
+                if (compChoice === 1) setFlag("YOU LOSE")
+                if (compChoice === 2) setFlag("YOU WIN")
+                break
+            case 1:
+                if (compChoice === 0) setFlag("YOU WIN")
+                if (compChoice === 1) setFlag("TIE")
+                if (compChoice === 2) setFlag("YOU LOSE")
+                break
+            case 2:
+                if (compChoice === 0) setFlag("YOU LOSE")
+                if (compChoice === 1) setFlag("YOU WIN")
+                if (compChoice === 2) setFlag("TIE")
+                break
+        }
+        setClickToggle(!clickToggle);
+    }
+
+    //Rerender status on clickToggle change hook
+    useEffect(() => {
+        if (flag === "YOU WIN") setUserScore(userScore + 1)
+        if (flag === "YOU LOSE") setCompScore(compScore + 1)
+    }, [clickToggle])
+
+
     return (
         <div id='game-div'>
             <div id="choice-picker-div">
@@ -21,7 +53,7 @@ const GameBody = () => {
                 <GameCard Result={handleResult} Type={1} Image={"./paper.png"} />
                 <GameCard Result={handleResult} Type={2} Image={"./scissors.png"} />
             </div>
-            <Score Score={[userScore, compScore]} />
+            <Score Flag={flag} Score={[userScore, compScore]} />
         </div>
     )
 }
@@ -29,9 +61,9 @@ const Score = (props) => {
 
     return (
         <div id="score-div">
-            <h3 id="h3-pop">Computer Score : {props.Score[0]}</h3>
-            <h3> {"<|>"} </h3>
-            <h3 id="h3-pop">Your Score : {props.Score[1]}</h3>
+            <h3 id="h3-pop">Computer Score : {props.Score[1]}</h3>
+            <h3> {"<" + props.Flag + ">"} </h3>
+            <h3 id="h3-pop">Your Score : {props.Score[0]}</h3>
         </div>)
 }
 const GameCard = (props) => {
